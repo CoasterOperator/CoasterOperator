b5e4f7dd893551c8b54eff94a93758cdd655fbf3
CoasterOperator <coasteroperator@gmail.com>
2024-09-07 14:24:18 -0400

    e

---
+++ Scripts/Accounts.js
@@ -0,0 +1,21 @@
+const { default: firebase } = require("firebase/compat/app");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBCOBG9DxKMA9pkWq1c-ANOAbKGHD4lPlg",
+    authDomain: "coasteroperatorweb.firebaseapp.com",
+    projectId: "coasteroperatorweb",
+    storageBucket: "coasteroperatorweb.appspot.com",
+    messagingSenderId: "828344346686",
+    appId: "1:828344346686:web:d8a8ae2eadecb82f46896f",
+    measurementId: "G-0THBL04QM8"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth()
+const database = firebase.database()
+
+function register () {
+    
+}
